4aa88a239062a25a0187aa355a80826c0b343ea7
winoffrg <rohangupta1528@gmail.com>
2025-09-07 00:05:05 +0530

    fix: SEO, metadata and OG

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://winoffrg.dev",
+  integrations: [],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
